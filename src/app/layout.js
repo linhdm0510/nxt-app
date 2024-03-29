4787c9ae3f4078e5c90988e5bc073ee7e3dba8f9
linhdm <linhdm@vmogroup.com>
2024-03-29 17:29:27 +0700

    feat(frontend): create ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/components/buttons/styles.scss"
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}> <AntdRegistry>{children}</AntdRegistry></body>
     </html>
   );
 }
